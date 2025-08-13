d5e698f7917b5d228bff11642fd42b950913b922
Michel Quiros Chaves <michelxquiros@gmail.com>
2025-08-12 20:55:40 -0600

    creacion grafica de login y menu de usuarios

---
+++ SistemaEducativo.Frontend/Pages/frontend/estudiantes/Index_estudiante.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaEducativo.Frontend.Pages.frontend
+{
+    public class Index_estudianteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- SistemaEducativo.Frontend/Program.cs
+++ SistemaEducativo.Frontend/Program.cs
@@ -1,15 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Frontend/Login"; // Página de login /
+        options.AccessDeniedPath = "/Frontend/AccesoDenegado"; // (opcional) página si no tiene permisos
+    });
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,7 +24,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapGet("/", () => Results.Redirect("/Frontend/Login")); //Frontend/Login
 
 app.MapRazorPages();
 
